45a069117db89ebc2989f00b20fc907f0ac4fe20
Fred Jiang <jiangf@shinetechsoftware.com>
2025-10-21 06:29:21 +0800

    add test

---
+++ ToDo.API/Todo.Application/CalService.cs
@@ -0,0 +1,10 @@
+﻿namespace Todo.Application
+{
+    public class CalService
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ ToDo.API/Todo.UnitTests/CalServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Todo.Application;
+using Xunit;
+
+namespace Todo.UnitTests
+{
+    public class CalServiceTests
+    {
+        [Fact]
+        public void AddTest_Succsss()
+        {
+            // Arrange
+            var cal = new CalService();
+
+            // Act
+            var result = cal.Add(1, 2);
+
+            // Assert
+            Assert.Equal(3, result);
+        }
+    }
+}
