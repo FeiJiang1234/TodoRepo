368b17415ea49fc12caaf491d9df939b9dbd8875
Fred Jiang <jiangf@shinetechsoftware.com>
2025-10-21 09:06:25 +0800

    cat delegate

--- ToDo.API/Todo.Application/MouseRun.cs
+++ ToDo.API/Todo.Application/MouseRun.cs
@@ -1,13 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Todo.Application
+﻿namespace Todo.Application
 {
+    public class CatShoutEventArgs : EventArgs
+    {
+        public string Name { get; set; }
+    }
+
     public class Cat
     {
-        //public delegate 
+        public delegate void CatShoutHandler(object sender, CatShoutEventArgs args);
+        public event CatShoutHandler CatShouted;
+
+        public void Shout()
+        {
+            Console.WriteLine("Cat Shout");
+
+            var e = new CatShoutEventArgs
+            {
+                Name = "Kitty"
+            };
+            CatShouted(this, e);
+        }
+    }
+
+    public class Mouse
+    {
+        public void Run(object sender, CatShoutEventArgs args)
+        {
+            Console.WriteLine("Run");
+        }
     }
 }
---
+++ ToDo.API/Todo.UnitTests/CatTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Todo.Application;
+using Xunit;
+
+namespace Todo.UnitTests
+{
+    public class CatTest
+    {
+        [Fact]
+        public void Cat_Tests()
+        {
+            var cat = new Cat();
+
+            var mouse = new Mouse();
+            cat.CatShouted += mouse.Run;
+
+            cat.Shout();
+        }
+    }
+}
